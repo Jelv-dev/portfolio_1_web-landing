222bc261e0a8b82d566604b1abb1ddbbe49b6cff
jelv-dev <jelv8@outlook.com>
2025-11-09 20:24:53 +0100

    fix: solucion definitiva de rutas absolutas para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,12 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  // 'site' se comenta para evitar conflictos con rutas relativas.
-  // site: 'https://jelv-dev.github.io/',
-  base: './', // Fuerza a Astro a generar rutas relativas para todos los assets.
+  site: 'https://jelv-dev.github.io/',
+  base: '/portfolio_1_web-landing',
   
   // Las integraciones deben estar aquí, correctamente llamadas como funciones
   integrations: [
     tailwind(),
     sitemap()
   ],
-
-  // Configuración de Vite para optimización de build (independiente de assetsPrefix)
-  vite: {
-    build: {
-      minify: true, // Asegura la minificación de JS/CSS
-      cssCodeSplit: false, // Combina todo el CSS en un solo archivo
-    },
-  },
 });
